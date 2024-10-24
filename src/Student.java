1eec250e6026a7cce6a17b0c810ab9e8e48154e4
Shravani_Parte <27parteshravani@gmail.com>
2024-10-24 23:07:02 +0530

    student part

--- src/Student.java
+++ src/Student.java
@@ -1,45 +1,38 @@
 package src;
 
-
 import java.util.ArrayList;
 
-
-public class Student extends User {
+public class Student extends User {   //inheritance from User class on using 'extends' keyword
     private ArrayList<Subject> subjects; //arraylist concept
 
-
-    public Student(String userId, String name) {  //use of super keyword to set student details
-        super(userId, name);
-        this.subjects = new ArrayList<>();
+    public Student(String userId, String name) {  
+        super(userId, name); //use of super keyword to invoke parent class constructoe
+        this.subjects = new ArrayList<>();     //use of arraylist
     }
-
 
     public void addSubject(Subject subject) {  //allot subject to student
         subjects.add(subject);
         System.out.println("Added subject " + subject.getSubjectName() + " for " + name);
     }
 
-
     public void markWeeklyAttendance(String subjectName, int weekNumber, int attendedLectures) {
         for (Subject subject : subjects) {  //use of for-each loop
             if (subject.getSubjectName().equals(subjectName)) {  
-                subject.markWeeklyAttendance(weekNumber, attendedLectures);  //mark wwekly attendance
+                subject.markWeeklyAttendance(weekNumber, attendedLectures);  //mark wwekly attendance 
                 return;
             }
         }
         System.out.println("Subject " + subjectName + " not found for " + name);
     }
 
-
-    @Override
-    public void viewAttendance() {
-        System.out.println("Attendance report for " + name + ":");   //get attendance report for student
+    @Override     //abstract function from parent abstract class is now defined
+    public void viewAttendance() {  
+        System.out.println("Attendance report for " + name + ":");   //get attendance report for student 
         for (Subject subject : subjects) {
-            System.out.printf("Subject: %s, Attendance: %.2f%%\n",
+            System.out.printf("Subject: %s, Attendance: %.2f%%\n", 
                                subject.getSubjectName(), subject.getAttendancePercentage());
         }
     }
-
 
     public void generateMonthlyAttendanceReport() { //Include eligibility in report
         System.out.println("Monthly Attendance Report for " + name + ":");
@@ -47,8 +40,7 @@ public class Student extends User {
             double attendancePercentage = subject.getAttendancePercentage();
             String eligibility = subject.isEligible() ? "Eligible" : "Not Eligible";
 
-
-            System.out.printf("Subject: %s, Attendance: %.2f%%, Status: %s\n",
+            System.out.printf("Subject: %s, Attendance: %.2f%%, Status: %s\n", 
                               subject.getSubjectName(), attendancePercentage, eligibility);
         }
     }
--- src/Subject.java
+++ src/Subject.java
@@ -1,12 +1,10 @@
 package src;
 
-
 public class Subject {   // 'final' keyword because these parameters are supposed to be fixed throughout
     private final String subjectName;
     private final int totalLecturesPerWeek;
     private final int[] attendedLecturesPerWeek;
     private static final int WEEKS_IN_MONTH = 4;
-
 
     public Subject(String subjectName, int totalLecturesPerWeek) {  //this is a parameterised constructor aka 'setters'
         this.subjectName = subjectName;  //Set the values of the class varaibles using 'this' keyword
@@ -14,11 +12,9 @@ public class Subject {   // 'final' keyword because these parameters are suppose
         this.attendedLecturesPerWeek = new int[WEEKS_IN_MONTH];
     }
 
-
     public String getSubjectName() {    //return the subject name by 'getters'
         return subjectName;
     }
-
 
     public void markWeeklyAttendance(int weekNumber, int attendedLectures) {  //set number of lectures attended per week
         if (weekNumber >= 1 && weekNumber <= WEEKS_IN_MONTH) {
@@ -29,20 +25,17 @@ public class Subject {   // 'final' keyword because these parameters are suppose
         }
     }
 
-
     public int getTotalLecturesForMonth() {      //get the total no. of lectures conducted per month
         return totalLecturesPerWeek * WEEKS_IN_MONTH;
     }
 
-
     public int getTotalAttendedLecturesForMonth() {  //get the total no. of lectures attended by student per month
         int totalAttended = 0;
-        for (int i = 0; i < WEEKS_IN_MONTH; i++) {    
-            totalAttended += attendedLecturesPerWeek[i];  
+        for (int i = 0; i < WEEKS_IN_MONTH; i++) {     
+            totalAttended += attendedLecturesPerWeek[i];   
         }
         return totalAttended;
     }
-
 
     public double getAttendancePercentage() {  //calculate attendance percentage
         int totalAttended = getTotalAttendedLecturesForMonth();
@@ -50,10 +43,7 @@ public class Subject {   // 'final' keyword because these parameters are suppose
         return (double) totalAttended / totalLectures * 100;
     }
 
-
     public boolean isEligible() {   //Check for exam eligibilty
-        return getAttendancePercentage() >= 75.0;  //return T or eligible if greater than 75%
+        return getAttendancePercentage() >= 75.0;  //return T or eligible if greater than 75% 
     }
 }
-
-
