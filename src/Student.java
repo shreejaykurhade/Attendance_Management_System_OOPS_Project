06946715e1b3c7d83e90de3798bcfbb6c403fdf3
shreejaykurhade <kshreejay@gmail.com>
2024-10-24 21:17:13 +0530

    Update Comments

--- src/Student.java
+++ src/Student.java
@@ -1,46 +1,54 @@
 package src;
 
+
 import java.util.ArrayList;
 
-public class Student extends User {
-    private ArrayList<Subject> subjects;
 
-    public Student(String userId, String name) {
+public class Student extends User {
+    private ArrayList<Subject> subjects; //arraylist concept
+
+
+    public Student(String userId, String name) {  //use of super keyword to set student details
         super(userId, name);
         this.subjects = new ArrayList<>();
     }
 
-    public void addSubject(Subject subject) {
+
+    public void addSubject(Subject subject) {  //allot subject to student
         subjects.add(subject);
         System.out.println("Added subject " + subject.getSubjectName() + " for " + name);
     }
 
+
     public void markWeeklyAttendance(String subjectName, int weekNumber, int attendedLectures) {
-        for (Subject subject : subjects) {
-            if (subject.getSubjectName().equals(subjectName)) {
-                subject.markWeeklyAttendance(weekNumber, attendedLectures);
+        for (Subject subject : subjects) {  //use of for-each loop
+            if (subject.getSubjectName().equals(subjectName)) {  
+                subject.markWeeklyAttendance(weekNumber, attendedLectures);  //mark wwekly attendance
                 return;
             }
         }
         System.out.println("Subject " + subjectName + " not found for " + name);
     }
 
+
     @Override
     public void viewAttendance() {
-        System.out.println("Attendance report for " + name + ":");
+        System.out.println("Attendance report for " + name + ":");   //get attendance report for student
         for (Subject subject : subjects) {
-            System.out.printf("Subject: %s, Attendance: %.2f%%\n", 
+            System.out.printf("Subject: %s, Attendance: %.2f%%\n",
                                subject.getSubjectName(), subject.getAttendancePercentage());
         }
     }
 
-    public void generateMonthlyAttendanceReport() {
+
+    public void generateMonthlyAttendanceReport() { //Include eligibility in report
         System.out.println("Monthly Attendance Report for " + name + ":");
         for (Subject subject : subjects) {
             double attendancePercentage = subject.getAttendancePercentage();
             String eligibility = subject.isEligible() ? "Eligible" : "Not Eligible";
 
-            System.out.printf("Subject: %s, Attendance: %.2f%%, Status: %s\n", 
+
+            System.out.printf("Subject: %s, Attendance: %.2f%%, Status: %s\n",
                               subject.getSubjectName(), attendancePercentage, eligibility);
         }
     }
--- src/Subject.java
+++ src/Subject.java
@@ -1,49 +1,59 @@
 package src;
 
-public class Subject {
+
+public class Subject {   // 'final' keyword because these parameters are supposed to be fixed throughout
     private final String subjectName;
     private final int totalLecturesPerWeek;
     private final int[] attendedLecturesPerWeek;
     private static final int WEEKS_IN_MONTH = 4;
 
-    public Subject(String subjectName, int totalLecturesPerWeek) {
-        this.subjectName = subjectName;
+
+    public Subject(String subjectName, int totalLecturesPerWeek) {  //this is a parameterised constructor aka 'setters'
+        this.subjectName = subjectName;  //Set the values of the class varaibles using 'this' keyword
         this.totalLecturesPerWeek = totalLecturesPerWeek;
         this.attendedLecturesPerWeek = new int[WEEKS_IN_MONTH];
     }
 
-    public String getSubjectName() {
+
+    public String getSubjectName() {    //return the subject name by 'getters'
         return subjectName;
     }
 
-    public void markWeeklyAttendance(int weekNumber, int attendedLectures) {
+
+    public void markWeeklyAttendance(int weekNumber, int attendedLectures) {  //set number of lectures attended per week
         if (weekNumber >= 1 && weekNumber <= WEEKS_IN_MONTH) {
             attendedLecturesPerWeek[weekNumber - 1] = attendedLectures;
             System.out.println("Marked attendance for week " + weekNumber + " in subject: " + subjectName);
         } else {
-            System.out.println("Invalid week number.");
+            System.out.println("Invalid week number.");  //if week no. is entered wrong
         }
     }
 
-    public int getTotalLecturesForMonth() {
+
+    public int getTotalLecturesForMonth() {      //get the total no. of lectures conducted per month
         return totalLecturesPerWeek * WEEKS_IN_MONTH;
     }
 
-    public int getTotalAttendedLecturesForMonth() {
+
+    public int getTotalAttendedLecturesForMonth() {  //get the total no. of lectures attended by student per month
         int totalAttended = 0;
-        for (int i = 0; i < WEEKS_IN_MONTH; i++) {
-            totalAttended += attendedLecturesPerWeek[i];
+        for (int i = 0; i < WEEKS_IN_MONTH; i++) {    
+            totalAttended += attendedLecturesPerWeek[i];  
         }
         return totalAttended;
     }
 
-    public double getAttendancePercentage() {
+
+    public double getAttendancePercentage() {  //calculate attendance percentage
         int totalAttended = getTotalAttendedLecturesForMonth();
         int totalLectures = getTotalLecturesForMonth();
         return (double) totalAttended / totalLectures * 100;
     }
 
-    public boolean isEligible() {
-        return getAttendancePercentage() >= 75.0;
+
+    public boolean isEligible() {   //Check for exam eligibilty
+        return getAttendancePercentage() >= 75.0;  //return T or eligible if greater than 75%
     }
 }
+
+
